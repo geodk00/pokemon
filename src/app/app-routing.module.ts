9155fc7b1c555702efcd11032ef8ee61695aaa51
geodk00 <tor@leeberg.dk>
2021-03-12 14:42:19 +0100

    feat: set up routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginContainer } from './features/login/containers/login/login.container';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/login'
+  },
+  {
+    path: 'login',
+    component: LoginContainer
+  }/*,
+  {
+    path: 'pokemon/:name',
+    component: PokemonDetailView
+  },
+  {
+    path: '**',
+    component: NotFoundView
+  }
+  */
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
