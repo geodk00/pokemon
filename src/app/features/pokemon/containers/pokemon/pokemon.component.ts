013b992dc057c79b3a54760d0fb49876c63259e2
geodk00 <tor@leeberg.dk>
2021-03-13 18:57:43 +0100

    feat: add functionality to pokemon container

--- src/app/features/pokemon/containers/pokemon/pokemon.component.ts
+++ src/app/features/pokemon/containers/pokemon/pokemon.component.ts
@@ -1,8 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Pokemon } from "src/app/models/pokemon.model";
+import { PokemonService } from "../../services/pokemon/pokemon.service";
 
 @Component({
     selector: 'app-pokemon-container',
     templateUrl: './pokemon.component.html'
 })
 
-export class PokemonContainer {}
+export class PokemonContainer implements OnInit {
+
+    constructor(private readonly pokemonService: PokemonService) {
+
+    }
+
+    get pokemon(): Pokemon[] {
+        return this.pokemonService.pokemon
+    }
+
+    ngOnInit(): void {
+        this.pokemonService.fetchPokemon();
+          
+    }
+}
