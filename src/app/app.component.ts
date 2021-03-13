4f476dff1a7ed3baa654cca097deff3375e6b542
geodk00 <tor@leeberg.dk>
2021-03-13 12:04:06 +0100

    refactor: remove localstorage from AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,27 +8,6 @@ import { LocalStorageService } from './features/login/services/local-storage/loc
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy{
+export class AppComponent {
   title = 'pokemon';
-
-  trainer$: Observable<string>
-  trainerSubscription: Subscription
-  trainer: string
-
-  constructor(private localStorageService: LocalStorageService) {
-
-  }
-
-  ngOnInit(): void {
-    this.trainerSubscription = this.localStorageService.getTrainer().subscribe((event) => this.trainer = event);
-  }
-
-  ngOnDestroy(): void {
-    this.trainerSubscription.unsubscribe();
-  }
-
-  onClick(input: string) {
-   console.log(input)
-   this.localStorageService.setTrainer(input)
-  }
 }
