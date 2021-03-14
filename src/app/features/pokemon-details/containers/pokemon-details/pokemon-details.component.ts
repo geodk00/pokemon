54d8ec5fbe0aa6ef1efea766ed64f9888dd70fab
geodk00 <tor@leeberg.dk>
2021-03-14 15:11:28 +0100

    feat: add collect button to pokemon details

--- src/app/features/pokemon-details/containers/pokemon-details/pokemon-details.component.ts
+++ src/app/features/pokemon-details/containers/pokemon-details/pokemon-details.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { LocalStorageService } from "src/app/features/login/services/local-storage/local-storage.service";
 import { Pokemon } from "src/app/models/pokemon.model";
+import { environment } from "src/environments/environment";
 import { PokemonDetailsService } from "../../services/pokemon-details.service";
+
+const { imageURL } = environment
 
 @Component({
     selector: 'app-pokemon-details',
@@ -14,7 +18,8 @@ export class PokemonDetailsContainer implements OnInit{
 
     constructor(
         private readonly route: ActivatedRoute, 
-        private readonly pokemonDetailsService: PokemonDetailsService) {
+        private readonly pokemonDetailsService: PokemonDetailsService,
+        private readonly localStorageService: LocalStorageService) {
         this.pokemonName = this.route.snapshot.paramMap.get('name');
     }
 
@@ -24,5 +29,15 @@ export class PokemonDetailsContainer implements OnInit{
 
     get pokemon(): Pokemon {
         return this.pokemonDetailsService.pokemon;
+    }
+
+    handleCollect() {
+        console.log('collecting pokemon')
+        const collectedPokemon :Pokemon= {
+            name: this.pokemonDetailsService.pokemon.name,
+            url: `${imageURL}//${ this.pokemonDetailsService.pokemon.id }.png`,
+            id: this.pokemonDetailsService.pokemon.id
+        }
+        this.localStorageService.addPokemon(collectedPokemon)
     }
 }
